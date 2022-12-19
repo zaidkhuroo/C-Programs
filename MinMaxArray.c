93b1f1432b747a789d32198d966a0a9447805384
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-19 13:11:17 +0530

    Add files via upload

---
+++ MinMaxArray.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],i,n,MAX,MIN;
+    printf("Enter size of the array");
+    scanf("%d", &n);
+    
+    printf("Enter Elements in array");
+    for(i=0;i<=n;i++)
+    {
+    scanf("%d", &a[i]);
+    }
+    MAX=MIN=a[0];
+for(i=1;i<n;i++)
+{
+    if(MIN>a[i])
+    MIN=a[i];
+    if(MAX<a[i])
+    MAX=a[i];
+}
+printf("Max element is %d", MAX);
+printf("Max element is %d", MIN);
+ return 0;
+}
---
+++ firstFile.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    FILE *fp=NULL;
+    char str[50];
+    int i;
+    int a= 10;
+    char ch='b';
+    fp=fopen("abc.txt","w");
+    if (fp==NULL)
+    {
+        printf("no file");
+        exit(1);
+    }
+    printf("Enter the string");
+    gets(str);
+
+    fprintf(fp,"%d %s %c", a,str,ch);
+    //for(i=0;i!=strlen(str);i++)
+    //fputs(str[i],fp);
+    fclose(fp);
+}
