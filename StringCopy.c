60c752b027e57d6b18c4202c2aada41463c693aa
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-20 00:38:05 +0530

    Create StringCopy.c

---
+++ StringCopy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char ch[05]={'b','c','d','e'};
+    char ch2[05];
+    strcpy(ch2,ch);
+    printf("the final string is %s", ch2);
+    return 0;
+}
