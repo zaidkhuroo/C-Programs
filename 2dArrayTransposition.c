9925ee8431b1dfa32a11f70ca0fff7e6ae590d71
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-21 00:42:03 +0530

    Add files via upload

---
+++ 2dArrayTransposition.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][3], i, j, sum = 0;
+
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 3; j++)
+    {
+        {
+            printf("Enter the %d element in array\n", i);
+            scanf("%d", &a[i][j]);
+        }
+    }
+printf("Original Matrix\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t",a[i][j]);
+           // sum = sum + a[i][j];
+        }
+    }
+        printf("\n");
+        printf("Transposed Matrix\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d\t",a[j][i]);
+           // sum = sum + a[i][j];
+        }
+        printf("\n");
+    }
+     // printf("\nThe sum of elements of the array is %d\n", sum);
+}
