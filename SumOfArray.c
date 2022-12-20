008915e3104fb49211c4c6d2b62b7434479f2ce5
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-20 16:30:18 +0530

    Add files via upload

---
+++ SumOfArray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a[3];
+    int a2[3],i,sum=0;
+    int new[3];
+
+    for(i=0;i<=3;i++)
+    {
+        printf("enter element");
+scanf("%d", &a[i]);
+    }
+     printf("2nd array\n");
+    for(i=0;i<=3;i++)
+    {
+        printf("enter element");
+scanf("%d", &a2[i]);
+    }
+    for(i=0;i<=3;i++)
+    {
+    new[i]=a[i] + a2[i];
+    printf("the sum is: %d\n", new[i]);
+}
+}
