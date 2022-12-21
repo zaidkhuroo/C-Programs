ec9a17b83d3f0ece995d785afdffae308fec8151
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-21 19:32:10 +0530

    Add files via upload

---
+++ FunWithArgReturnType.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int adnan(int, int);
+int main()
+{
+    int a, b, s;
+    printf("enter two numbers\n");
+    scanf("%d %d", &a, &b);
+    s = adnan(a, b);
+    printf("the sum is %d", s);
+}
+
+int adnan(int x, int y)
+{
+    int sum = 0;
+    sum = x + y;
+    return (sum);
+}
