eecba4e38d3fd23581ce337226401b847d66c12c
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-21 17:27:53 +0530

    Add files via upload

---
+++ FunctionCallByRefrence.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void fun(int*, int*);
+void main(int x, int y)
+{
+    x = 10;
+    y = 20;
+    printf("x=%d,y=%d\n", x, y);
+    fun(&x, &y);
+}
+void fun(int *x, int *y)
+{
+    *x = 5;
+    *y = 2;
+    printf("x=%d,y=%d\n", *x, *y);
+}
