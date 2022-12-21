b5b841037b6ecb666584c676ccc23d3d639ad3d0
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-21 16:02:10 +0530

    Add files via upload

---
+++ FunctionCallByValue.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void fun(int,int);
+void main(int x,int y)
+{
+    x=10;
+    y=20;
+fun(x,y);
+printf("x=%d,y=%d\n",x,y);
+}
+void fun(int x,int y)
+{
+    x=5;
+    y=2;
+    printf("x=%d,y=%d\n",x,y);
+}
