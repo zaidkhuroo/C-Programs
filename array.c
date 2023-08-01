cadac1adc5f0cd93c124a21e8815550299d55d9f
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-08-01 22:36:31 +0530

    Update array.c

--- array.c
+++ array.c
@@ -4,7 +4,7 @@ int main()
     int a[20],i;
     for(i=0;i<=3;i++)
     { 
-       printf("enter the %d element\n", i);
+       printf("enter the %d element\n", i); 
         scanf("%d", &a[i]);
     }
     printf("\nOriginal: ");
